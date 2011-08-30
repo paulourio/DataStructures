8fa4569e441ac3e0f7cd937fdefc017985a6c7e3
Paulo Roberto Urio <jimmyskull@gmail.com>
2011-08-29 21:35:55 -0400

    Identação corrigida

--- arvore_binaria/teste.c
+++ arvore_binaria/teste.c
@@ -4,35 +4,35 @@
 
 static void print_node(const int v)
 {
-    printf(" %d", v);
+	printf(" %d", v);
 }
 
 
 int main(void)
 {
-    void    *arvore = NULL;
-    int valores[] = {15, 5, 3, 12, 10, 13, 6, 7, 16, 20, 18, 23, 0};
-    int i;
+	void    *arvore = NULL;
+	int valores[] = {15, 5, 3, 12, 10, 13, 6, 7, 16, 20, 18, 23, 0};
+	int i;
 
-    puts("Adicionando..");
-    for (i=0; valores[i] != 0; i++)
-        tree_insert(&arvore, valores[i]);
+	puts("Adicionando..");
+	for (i=0; valores[i] != 0; i++)
+	tree_insert(&arvore, valores[i]);
 
-    puts("Imprimindo..");
-    tree_walk(arvore, print_node, WALK_INORDER);
-    puts("");
-    printf("Maior: %d\n", tree_max_value(arvore));
-    printf("Menor: %d\n", tree_min_value(arvore));
-    printf("Sucessor de 5: %d\n", tree_successor_value(arvore, 5));
-    printf("Sucessor de 15: %d\n", tree_predecessor_value(arvore, 15));
+	puts("Imprimindo..");
+	tree_walk(arvore, print_node, WALK_INORDER);
+	puts("");
+	printf("Maior: %d\n", tree_max_value(arvore));
+	printf("Menor: %d\n", tree_min_value(arvore));
+	printf("Sucessor de 5: %d\n", tree_successor_value(arvore, 5));
+	printf("Sucessor de 15: %d\n", tree_predecessor_value(arvore, 15));
 
-    puts("Removendo nó..");
-    tree_delete(&arvore, 16);
-    tree_walk(arvore, print_node, WALK_INORDER);
-    puts("");
+	puts("Removendo nó..");
+	tree_delete(&arvore, 16);
+	tree_walk(arvore, print_node, WALK_INORDER);
+	puts("");
 
 
-    puts("Liberando...");
-    tree_free(&arvore);
-    return EXIT_SUCCESS;
+	puts("Liberando...");
+	tree_free(&arvore);
+	return EXIT_SUCCESS;
 }
