c23fab521ba6049a7943a57a52924bd59cb7674a
Paulo Roberto Urio <jimmyskull@gmail.com>
2011-09-25 19:13:59 -0400

    Huffman: Static stack test
    Improved static test
    Signed-off-by: Paulo Roberto Urio <jimmyskull@gmail.com>

--- huffman_code/spec/stack_spec.c
+++ huffman_code/spec/stack_spec.c
@@ -7,14 +7,17 @@ int main(void)
 {
 	void *stack = stack_new(3);
 	
+	assert(stack_isempty(stack) == 1);
 	assert(stack_pop(stack) == EMPTY_STACK);
 	stack_push(stack, 'a');
 	stack_push(stack, 'b');
 	stack_push(stack, 'c');
+	assert(stack_isempty(stack) == 0);
 	assert(stack_pop(stack) == 'c');
 	assert(stack_pop(stack) == 'b');
 	assert(stack_pop(stack) == 'a');
 	assert(stack_pop(stack) == EMPTY_STACK);
+	assert(stack_isempty(stack) == 1);
 	
 	stack = stack_free(stack);
 	return EXIT_SUCCESS;
